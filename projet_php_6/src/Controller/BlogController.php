b34ca50374a816460a337d3d3bd961989a9cdd23
curtisbreezy <mourad.kheloui@gmail.com>
2018-10-21 22:06:36 +0200

    installation de symfony, création des bdd user et articles, template twig sur une base bootstrap, formulaire de contact et accueil crée

---
+++ projet_php_6/src/Controller/BlogController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Article;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistance\ObjectManager;
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index()
+    {
+		$repo = $this->getDoctrine()->getRepository(Article::class);
+		
+		$articles = $repo->findAll();
+		
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+			'articles' => $articles,
+        ]);
+    }
+	
+	/**
+	 * @Route("/", name="home")
+	 */
+	public function home()
+	{	
+		return $this->render('blog/home.html.twig',
+		[
+			'title' => "Site propulsé par Symfony"
+		]);
+	}
+	
+	/**
+	 * @Route("/blog/forum/{id}", name="forum")
+	 */
+	
+	public function forum($id)
+	{	
+		$repo = $this->getDoctrine()->getRepository(Article::class);
+		
+		$articles = $repo->find($id);
+		return $this->render('blog/forum.html.twig',
+		[
+			'articles' => $articles,
+		]);
+	}
+	/**
+	 * @Route("/blog/contact", name="contact")
+	 */
+	public function contact()
+	{	
+		return $this->render('blog/contact.html.twig',
+		[
+			'title' => "Site propulsé par Symfony"
+		]);
+
+	}
+	
+	/**
+	 * @Route("/blog/create", name="create")
+	 */
+	
+	public function create(Request $request)
+	{	
+		dump($request);
+		
+		if($request->request->count() > 0) {
+			$articles = new Article();
+			$articles->setTitle($request->request->get('title'))
+					->setContent($request->request->get('content'))
+					->setImage($request->request->get('image'));
+					
+			
+		}
+		
+		return $this->render('blog/create.html.twig');
+		
+
+	}
+}
